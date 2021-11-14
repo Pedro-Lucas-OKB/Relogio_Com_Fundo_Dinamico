00450c0c2bd831bf6a1059d7cd4e146d337baeec
Pedro Lucas <pedrogmobile2@gmail.com>
2021-11-14 20:58:01 -0300

    update files

--- src/script.js
+++ src/script.js
@@ -9,19 +9,19 @@ function load() {
     clock();
     if (hour >= 6 && hour < 12) { // Manhã
         msg.innerHTML = '<strong>Bom dia!</strong>';
-        img.src = 'imagens/manha.png';
+        img.src = '../imagens/manha.png';
         img.alt = 'alt="manhã do dia';
         window.document.body.style.background = '#E8D3C7';
         window.document.getElementsByTagName('h1')[0].style.color = '#000000';
         window.document.getElementById('ftr').style.color = '#000000';
     } else if (hour >= 12 && hour <= 18){ // Tarde
         msg.innerHTML = '<strong>Boa tarde!</strong>';
-        img.src = 'imagens/tarde.png';
+        img.src = '../imagens/tarde.png';
         img.alt = 'alt="tarde do dia';
         window.document.body.style.background = '#CA5A0E';
     } else { // Noite
         msg.innerHTML = '<strong>Boa noite!</strong>';
-        img.src = 'imagens/noite.png';
+        img.src = '../imagens/noite.png';
         img.alt = 'alt="noite do dia';
         window.document.body.style.background = '#171F2C';
     }    
@@ -41,4 +41,5 @@ function clock() {
     hr.innerHTML = `<strong>${hours}:${minutes}:${seconds}</strong>`;
 }
 
+//intervalo de chamada da função clock
 setInterval(clock, 100); 
